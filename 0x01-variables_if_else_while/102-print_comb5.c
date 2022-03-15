ed0de6bc8e19c32dcc627f1844ffda08a19c48fd
Joshua Ajayi <Joshayotundeaj@gmail.com>
2022-03-15 16:56:01 -0700

    Prints all possible combinations of two two-digit numbers with specifics

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,11 +15,11 @@ int main(void)
 		{
 			if (m > n)
 			{
-				putchar((n/10)+'0');
-				putchar((n%10)+'0');
+				putchar((n / 10)+'0');
+				putchar((n % 10)+'0');
 				putchar(' ');
-				putchar((m/10)+'0');
-				putchar((m%10)+'0');
+				putchar((m / 10)+'0');
+				putchar((m % 10)+'0');
 				if (n != 98 || m != 99)
 				{
 					putchar(',');
